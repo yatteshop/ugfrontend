4f7c473e45e0464f7d20ecb3f87b0babbaf70868
davyphilippe <nchodavyphilippe@gmail.com>
2026-06-01 18:54:27 +0200

    Fix Netlify deployment routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify()
 });
